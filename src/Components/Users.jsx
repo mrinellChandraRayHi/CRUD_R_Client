26fa74442b3a33ce282d37bcd62fdfac5bf3c64e
MrinellChandraRay <mrinellroy@gmail.com>
2024-04-26 17:45:56 +0600

    first commit

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -1,13 +1,33 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
-    const users=useLoaderData();
+    const loadedUsers=useLoaderData();
+    const [users, setUsers]=useState(loadedUsers);
+
+    const handleDelete=(_id)=>{
+        console.log(_id);
+        fetch(`http://localhost:5000/users/${_id}`,{
+            method:'DELETE'
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+            if(data.deletedCount>0){
+                alert('deleted Successfully');
+                const remaining=users.filter(user => user._id !==_id);
+                setUsers(remaining);
+            }
+        })
+    }
     return (
         <div>
-            <h2>{users.length}</h2>
+            {/* users====age alada silo */}
+            <h2>{users.length}</h2> 
             <div>
+                {/* users====age alada silo */}
                 {
-                    users.map(user=><p key={user._id}>{user.name} : {user.email}</p>)
+                    users.map(user=><p key={user._id}>{user.name} : {user.email} : {user._id} <button onClick={()=> handleDelete(user._id)}>X</button></p>)
                 }
             </div>
         </div>
